81e6cd8d29886b26cdf4fca7c87236b34ccc3695
Muhammad Talha Dar <talha.dar@invozone.com>
2022-06-16 12:28:43 +0500

    some stuff with variadics

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,21 @@ macro_rules! create_better_function_templace {
         }
     };
 }
-// generates a template function
+macro_rules! calculate {
+    // The pattern for a single `eval`
+    (eval $e:expr) => {{
+        {
+            let val: usize = $e; // Force types to be integers
+            println!("{} = {}", stringify!{$e}, val);
+        }
+    }};
+
+    // Decompose multiple `eval`s recursively
+    (eval $e:expr, $(eval $es:expr),+) => {{
+        calculate! { eval $e }
+        calculate! { $(eval $es),+ }
+    }};
+}
 
 fn main() {
     create_function!(some_finction);
@@ -69,4 +83,10 @@ fn main() {
     create_better_function_templace!(pub, say_hello_template; bool);
     say_hello_template(a);
     say_hello_template(b);
+
+    calculate! { // Look ma! Variadic `calculate!`!
+        eval 1 + 2,
+        eval 3 + 4,
+        eval (2 * 3) + 1
+    }
 }
